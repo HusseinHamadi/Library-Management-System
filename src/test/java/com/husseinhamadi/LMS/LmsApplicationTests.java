14230480b74333259d814fcb573f41cd8fa53062
HusseinHamadi <housseinhamadh@gmail.com>
2024-04-02 12:44:06 +0300

    create project

---
+++ src/test/java/com/husseinhamadi/LMS/LmsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.husseinhamadi.LMS;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LmsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
